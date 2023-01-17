d14cacf02b6b281de781b3792400b2622202ddf9
Guo Yunhe <i@guoyunhe.me>
2023-01-17 20:59:22 +0800

    pagination and filter

--- src/pages/game-list/index.tsx
+++ src/pages/game-list/index.tsx
@@ -1,14 +1,36 @@
-import { Container, Grid } from '@mui/material';
+import { Box, Checkbox, Container, FormControlLabel, Grid, Pagination } from '@mui/material';
+import { ChangeEvent, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import useSWR from 'swr';
 import Game from '../../types/models/Game';
 import PaginatedResult from '../../types/PaginatedResult';
 import GameCard from './GameCard';
 
 export default function GameList() {
-  const { data, error } = useSWR<PaginatedResult<Game>>('games?hasBoxart=1', { refreshInterval: 0 });
+  const { t } = useTranslation();
+  const [page, setPage] = useState(1);
+  const [noBoxartImage, setNoBoxartImage] = useState(false);
+  const { data } = useSWR<PaginatedResult<Game>>(`games?page=${page}${noBoxartImage ? '&noBoxartImage=1' : ''}`, {
+    refreshInterval: 0,
+  });
+
+  const handlePageChange = (e: ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  const handleNoBoxartImageChange = (e: ChangeEvent<HTMLInputElement>, checked: boolean) => {
+    setNoBoxartImage(checked);
+    setPage(1);
+  };
 
   return (
     <Container maxWidth="xl" sx={{ py: 3 }}>
+      <Box>
+        <FormControlLabel
+          control={<Checkbox checked={noBoxartImage} onChange={handleNoBoxartImageChange} />}
+          label={t('No boxart image')}
+        />
+      </Box>
       {data && (
         <Grid container spacing={3}>
           {data.data.map((game) => {
@@ -20,6 +42,7 @@ export default function GameList() {
           })}
         </Grid>
       )}
+      <Pagination page={page} count={data?.meta.lastPage} onChange={handlePageChange} />
     </Container>
   );
 }

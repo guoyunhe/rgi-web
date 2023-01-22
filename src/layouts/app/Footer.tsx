b295cca9f93436f3b51dc5751092d125686a1acd
Guo Yunhe <i@guoyunhe.me>
2023-01-22 16:00:11 +0800

    Add GitHub and Telegram links to footer

--- src/layouts/app/Footer.tsx
+++ src/layouts/app/Footer.tsx
@@ -1,12 +1,21 @@
-import { Box, Container } from '@mui/material';
+import { Box, Container, Link } from '@mui/material';
 
 const year = new Date().getFullYear();
 
 export default function Footer() {
   return (
     <Box py={4}>
-      <Container maxWidth="xl">
-        &copy; {year} {import.meta.env.VITE_APP_NAME}
+      <Container maxWidth="xl" sx={{ display: 'flex', gap: 2 }}>
+        <Box>
+          &copy; {year} {import.meta.env.VITE_APP_NAME}
+        </Box>
+        <Box sx={{ flex: '1 1 auto' }} />
+        <Link href="https://t.me/retrogameindex" target="_blank">
+          Telegram
+        </Link>
+        <Link href="https://github.com/guoyunhe/rgi-web" target="_blank">
+          GitHub
+        </Link>
       </Container>
     </Box>
   );
